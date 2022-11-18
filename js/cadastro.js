796e902d57f6d8272dd4a400bba58108f0c9bca7
lucas <romancini12@gmail.com>
2022-11-18 03:49:17 -0300

    vamos la

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,70 +1,14 @@
-const senha = document.querySelector('.password')
-const c_senha = document.querySelector('.confirmpassword')
-const btn_enter = document.querySelector('.enterbutton')
-const btn_password = document.querySelector('.btnpass')
-const error = document.querySelector('.error')
-const user = document.querySelector('.user')
-const email = document.querySelector('.email') 
-const email_error = 0
+const user = document.getElementById("name");
+const register_button = document.getElementById("confirmbutton");
 
-btn_password.onclick = () =>
-{
-    if (senha.type == 'password')
+function registrar(){
+    const password = document.getElementById("password");
+    const confirm_password = document.getElementById("confirmPassword");
+    if(confirm_password.value == password.value)
     {
-        senha.type = 'text'
-        c_senha.type = 'text'
-        btn_password.src = '/imgs/ocultar.png'
-    }
-    else
-    {
-        senha.type = 'password'
-        c_senha.type = 'password'
-        btn_password.src = '/imgs/mostrar.png'
-    }
-}
-
-
-
-btn_enter.onclick = () =>
-{
-    if (senha.value != c_senha.value)
-    {
-        giveError(senha,'Senhas não coincidem');
-    }
-    if (senha.value == c_senha.value)
-    {
-        senha.style.border ='1px solid black'
-        c_senha.style.border ='1px solid black'
-    }
-    if (user.value == (''))
-    {
-        giveError(user,'Usuario não pode estar em branco')
-    }
-    if (user.value != '' && senha.value == c_senha.value)
-    {
-        user.style.border = ('1px solid black')
-        error.innerHTML = ('')
-    }
-    if (email.value == ('')){
-        giveError(email,'Email não pode estar em branco')
-    }
-    else if (!confirmEmail(email.value)){
-        giveError(email,'Email não é valido')
+        console.log("yes")
     }
     else{
-        email.style.border = ('1px solid black')
+        console.log("Nothing")
     }
-}
-
-function giveError(input, message){
-    error.innerHTML = (message);
-    input.style.border = '2px solid red'
-    if (input == senha){
-        senha.style.border = '2px solid red'
-        c_senha.style.border = '2px solid red'
-    }
-}
-
-function confirmEmail(email){
-    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
 }
